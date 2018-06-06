b3d670b98e5c3aa181fdaa32d1b1d40e870c0411
DESKTOP-V7V39VU\Shiga Yosuke <biology4suke4ga@hotmail.com>
2018-06-06 20:21:31 +0900

    Lesson 7 6/6分
    ランダムでブロックを生成。背景とブロックを左方向に移動するようにした

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -1,29 +1,58 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
 
 public class UnityChanController : MonoBehaviour
 {
     //アニメーションするためのコンポーネントを入れる
     Animator animator;
-    //地面の位置
-    private float groundLevel = -3.0f;
-	// Use this for initialization
-	void Start ()
-    {
-        //アニメーターのコンポーネントを取得する
-        this.animator = GetComponent<Animator>();
 
-	}
-	
-	// Update is called once per frame
-	void Update ()
+    //Unityちゃんを移動させるコンポーネントを入れる（追加）
+    Rigidbody2D rigid2D;
+
+    // 地面の位置
+    private float groundLevel = -3.0f;
+
+    // ジャンプの速度の減衰（追加）
+    private float dump = 0.8f;
+
+    // ジャンプの速度（追加）
+    float jumpVelocity = 20;
+
+    // Use this for initialization
+    void Start()
     {
-        //走るアニメーションを再生するためにAnimatorのパラメータを調節する
+        // アニメータのコンポーネントを取得する
+        this.animator = GetComponent<Animator>();
+        // Rigidbody2Dのコンポーネントを取得する（追加）
+        this.rigid2D = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // 走るアニメーションを再生するために、Animatorのパラメータを調節する
         this.animator.SetFloat("Horizontal", 1);
 
-        //着地しているかどうかを調べる
+        // 着地しているかどうかを調べる
         bool isGround = (transform.position.y > this.groundLevel) ? false : true;
-        this.animator.SetBool("IsGround", isGround);
-	}
+        this.animator.SetBool("isGround", isGround);
+
+        // 着地状態でクリックされた場合（追加）
+        if (Input.GetMouseButtonDown(0) && isGround)
+        {
+            // 上方向の力をかける（追加）
+            this.rigid2D.velocity = new Vector2(0, this.jumpVelocity);
+        }
+
+        // クリックをやめたら上方向への速度を減速する（追加）
+        if (Input.GetMouseButton(0) == false)
+        {
+            if (this.rigid2D.velocity.y > 0)
+            {
+                this.rigid2D.velocity *= this.dump;
+            }
+        }
+    }
 }
